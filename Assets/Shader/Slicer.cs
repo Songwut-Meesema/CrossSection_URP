7bb91a91f205106e61fba9a095302190a10d9666
Songwut-Meesema <SongwutWorking@gmail.com>
2026-02-19 02:39:53 +0700

    script update

--- Assets/Shader/Slicer.cs
+++ Assets/Shader/Slicer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FruitSlicer : MonoBehaviour
+public class Slicer : MonoBehaviour
 {
     [Header("Shader Settings")]
     [Range(-1f, 1f)] 
